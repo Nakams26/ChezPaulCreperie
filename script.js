8c1dafff1c62c70d64a86c3c5554a3492354168e
adrienpolselli <adrienpolselli@hotmail.com>
2023-03-06 09:49:57 -0500

    Fixing burger menu

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //1. Adding a burger menu in the header for the mobile view
 
 // Add a variable for li that  I need to display when clicking on the menu
-const dynamicLi = document.querySelector(".dynamicMenu");
+const header = document.querySelector("header");
 // Add a variable for the hamburger menu that I put in a div. this is the clickable element
-const hamburgerMenu = document.querySelector(".hamburgerMenu");
+const nav = document.querySelector("nav");
 
 // Add a a variable for button open
 const openMenu = document.querySelector(".menuOpen");
@@ -13,20 +13,21 @@ const closeMenu = document.querySelector(".menuClose");
 
 //Add a event listener to open menu when the burger menu is clicked
 
-hamburgerMenu.addEventListener("click", function () {
+header.addEventListener("click", function (event) {
   // If when you click the burger button, the menu doesn't contain showMenu as class, then I add the class (The class allow us to display the menu from the right with a transform translate X).  I also in the same time hide the open icon and display the close icon
-  if (!dynamicLi.classList.contains("showMenu")) {
-    dynamicLi.classList.add("showMenu");
-
+  if (event.target.classList[2]=== "menuOpen") {
+  
     closeMenu.style.color = "black";
     openMenu.style.display = "none";
+    nav.style.display = "block";
   }
   // If when you click the burger button, the menu  already contain showMenu as class, then I remove the class. This will hide the menu
-  else {
-    dynamicLi.classList.remove("showMenu");
+  else if (event.target.classList[2]=== "menuClose") {
+  
 
     //I also in the same time show the  open menu icon and hide the close menu
     closeMenu.style.color = "transparent";
     openMenu.style.display = "block";
+    nav.style.display = "none";
   }
 });
