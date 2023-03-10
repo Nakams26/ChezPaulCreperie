0a659015ad5950097130c6860f495ffff635f77e
adrienpolselli <adrienpolselli@hotmail.com>
2023-03-09 20:29:36 -0500

    Adding galettes firebase

--- scripts/script.js
+++ scripts/script.js
@@ -4,21 +4,75 @@ import {
   getDatabase,
   ref,
   onValue,
-  push,
-  update,
-  get,
-} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+} from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
 
 // Initialize Database content using the configured app
 const database = getDatabase(app);
 //Create a specific reference to the root of my db
 //This is the representation of our DB (this is how we hook into the DB and update and interact with it)
 const dbRef = ref(database);
-const dbGalettes = ref(database, `.galettes`);
-console.log(dbGalettes);
-const dbCrepes = ref(database, `/crepes`);
-const dbGourmandises = ref(database, `/gourmandises`);
-const dbDrinks = ref(database, `/drinks`);
+const dbGalettes = ref(database, `/galettes`);
+const ulGalette = document.querySelector(".flexGalette");
+console.log(ulGalette)
+
+onValue(dbGalettes, function (data) {
+  if (data.exists()) {
+    ulGalette.innerHTML = "";
+    const newGalettes = data.val();
+    for (let key in newGalettes) {
+      const name = newGalettes[key].name;
+      const price = newGalettes[key].price;
+      const text1 = newGalettes[key].text;
+      const text2 = newGalettes[key].text2;
+      const text3 = newGalettes[key].text3;
+      const text4 = newGalettes[key].text4;
+      const text5 = newGalettes[key].text5;
+      const text6 = newGalettes[key].text6;
+
+      const nameItem = document.createElement("h4");
+      nameItem.textContent = name;
+
+      const listItem = document.createElement("li");
+      listItem.classList.add("galette");
+
+      const priceItem = document.createElement("p");
+      priceItem.classList.add("price");
+      priceItem.textContent = price;
+
+      const firstText = document.createElement("p");
+      firstText.textContent = text1;
+
+      const secondText = document.createElement("p");
+      secondText.textContent = text2;
+
+      const thirdtext = document.createElement("p");
+      thirdtext.textContent = text3;
+
+      const fourthText = document.createElement("p");
+      fourthText.textContent = text4;
+
+      const fifthtext = document.createElement("p");
+      fifthtext.textContent = text5;
+
+      const sixthtext = document.createElement("p");
+      sixthtext.textContent = text6;
+
+      listItem.append(
+        nameItem,
+        priceItem,
+        firstText,
+        secondText,
+        thirdtext,
+        fourthText,
+        fifthtext,
+        sixthtext
+      );
+
+      ulGalette.appendChild(listItem)
+      
+    }
+  }
+});
 
 // Add a variable for header.
 const header = document.querySelector("header");
@@ -83,5 +137,4 @@ dropUp.addEventListener("click", function () {
   dropUp.style.color = "transparent";
   dropUp.style.zIndex = "0";
   dropDown.style.color = "black";
- 
 });
